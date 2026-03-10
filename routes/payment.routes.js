5791b4ae6046934dffb7ce7ed067f9793ea363c1
zhong xian <zhongxian16@gmail.com>
2026-03-10 18:57:15 +0800

    fix model case

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -3,7 +3,7 @@ const Stripe = require("stripe")
 
 const router = express.Router()
 
-const Booking = require("../models/Booking")
+const Booking = require("../models/booking")
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
