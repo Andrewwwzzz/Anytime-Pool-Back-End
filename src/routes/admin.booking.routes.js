7ed692512354a1ce06b566b58b03c13b52e29f7d
zhong xian <zhongxian16@gmail.com>
2026-03-10 20:53:20 +0800

    Update 2

--- src/routes/admin.booking.routes.js
+++ src/routes/admin.booking.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Booking = require("../models/booking");
+const Booking = require("../models/Booking");
 const authMiddleware = require("../middleware/auth.middleware");
 const roleMiddleware = require("../middleware/role.middleware");
 
