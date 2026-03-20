3dabd1905d10aaa7033d8efacac12594312accd8
zhong xian <zhongxian16@gmail.com>
2026-03-21 01:46:27 +0800

    Light Integration

--- src/models/table.js
+++ src/models/table.js
@@ -1,33 +1,39 @@
 const mongoose = require("mongoose");
 
-const tableSchema = new mongoose.Schema(
-  {
-    tableNumber: {
-      type: Number,
-      required: true,
-      unique: true
-    },
-
-    name: {
-      type: String,
-      required: true
-    },
-
-    basePrice: {
-      type: Number,
-      required: true
-    },
-
-    isActive: {
-      type: Boolean,
-      default: true
-    },
-
-    hardware_id: {
-      type: String
-    }
+const tableSchema = new mongoose.Schema({
+  tableNumber: {
+    type: Number,
+    required: true
   },
-  { timestamps: true }
-);
+
+  name: {
+    type: String,
+    required: true
+  },
+
+  basePrice: {
+    type: Number,
+    required: true
+  },
+
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+
+  hardware_id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  // 🔥 ADD THIS
+  manualOverride: {
+    type: String,
+    enum: ["ON", "OFF", null],
+    default: null
+  }
+
+}, { timestamps: true });
 
 module.exports = mongoose.model("Table", tableSchema);
